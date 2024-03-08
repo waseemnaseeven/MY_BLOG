12f82c5cf4649f41ef75a7f5eaf6a83becc15a04
waseemnaseeven <waseem.naseeven@gmail.com>
2024-03-08 18:00:27 +0100

    navbar downloading...

--- README.md
+++ README.md
@@ -1,5 +1,15 @@
 # MY_BLOG
 
+## Here is what i've learned
+
+```JavaScript
+<Nav.Link href="#Home" className={activeLink === "Home" ? "active navbar-link" : "navbar-link"} onClick={() => onUpdateActiveLink("Home")}>Home</Nav.Link>
+```
+- Il y a un composant Bootstrap qui fournit des liens dans une barre de navigation.
+- href="#Home", lorsque ce lien est clique, il va naviguer vers l'element avec l'ID "Home" sur la page.
+- onClick={() => onUpdateActiveLink("Home")}: C'est un gestionnaire d'événements qui est déclenché lorsque le lien est cliqué. Il appelle la fonction onUpdateActiveLink avec l'argument "Home". Cela pourrait être utilisé pour mettre à jour l'état de l'application, par exemple, pour indiquer quel lien est actuellement actif.
+
+
 ### Usefull commands
 
 - npm create vite@latest
--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,6 +1,13 @@
 import {React, useEffect, useState} from 'react';
 import { Navbar, Container, Nav } from 'react-bootstrap';
 import './navbar.css';
+import header from '../../assets/img/header-img.svg';
+import githubicon from '../../assets/img/github-icon.svg';
+import linkedinicon from '../../assets/img/linkedin-icon.svg';
+import instaicon from '../../assets/img/insta-icon.svg';
+import spotifyicon from '../../assets/img/spotify-icon.svg';
+import discordicon from '../../assets/img/discord-icon.svg';
+
 
 const navComponent = () => {
     const [activeLink, setActiveLink] = useState('Home');
@@ -19,27 +26,34 @@ const navComponent = () => {
         return () => window.removeEventListener('scroll', onScroll);
     }, []);
 
+    const onUpdateActiveLink = (link) => {
+        setActiveLink(link);
+    }
+
     return (
-        <Navbar bg="light" expand="lg" className={scrolled ? "scrolled": ""}>
+        <Navbar expand="lg" className={scrolled ? "scrolled": ""}>
             <Container>
                 <Navbar.Brand href="#Home">
-                    <img src={''} alt="Waseem Naseeven" />
+                    <img src={header} alt="Waseem Naseeven" />
                 </Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav">
                     <span className="navbar-toggler-icon"></span>
                 </Navbar.Toggle>
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="me-auto">
-                        <Nav.Link href="#About">About</Nav.Link>
-                        <Nav.Link href="#Projects">Projects</Nav.Link>
-                        <Nav.Link href="#Music">Music</Nav.Link>
-                        <Nav.Link href="#Contact">Contact</Nav.Link>
+                        <Nav.Link href="#Home" className={activeLink === "Home" ? "active navbar-link" : "navbar-link"} onClick={() => onUpdateActiveLink("Home")}>Home</Nav.Link>
+                        <Nav.Link href="#About" className={activeLink === "About" ? "active navbar-link" : "navbar-link"} onClick={() => onUpdateActiveLink("About")}>About</Nav.Link>
+                        <Nav.Link href="#Projects" className={activeLink === "Projects" ? "active navbar-link" : "navbar-link"} onClick={() => onUpdateActiveLink("Projects")}>Projects</Nav.Link>
+                        <Nav.Link href="#Music" className={activeLink === "Music" ? "active navbar-link" : "navbar-link"} onClick={() => onUpdateActiveLink("Music")}>Music</Nav.Link>
+                        <Nav.Link href="#Contact" className={activeLink === "Contact" ? "active navbar-link" : "navbar-link"} onClick={() => onUpdateActiveLink("Contact")}>Contact</Nav.Link>
                     </Nav>
                     <span className="navbar-text">
                         <div className="social-icons">
-                            <a href="#github"><img src={''} alt=""/></a>
-                            <a href="#linkedin"><img src={''} alt=""/></a>
-                            <a href="#instagram"><img src={''} alt=""/></a>
+                            <a href="#github"><img src={githubicon} alt="github"/></a>
+                            <a href="#linkedin"><img src={linkedinicon} alt="linkedin"/></a>
+                            <a href="#instagram"><img src={instaicon} alt="insta"/></a>
+                            <a href="#spotify"><img src={spotifyicon} alt="spotify"/></a>
+                            <a href="#discord"><img src={discordicon} alt="discord"/></a>
                             <a href="#patreon"><img src={''} alt=""/></a>
                         </div>
                         <button className="vvd" onClick={() => console.log('connect')}><span>Let's Connect</span></button>
